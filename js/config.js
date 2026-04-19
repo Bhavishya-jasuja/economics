d008c49237583060aa16d1e997c915535a524a70
Bhavishya-jasuja <jasujabhavishya@gmail.com>
2026-04-19 21:45:09 +0530

    update contact form and marks form script

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  * That script receives POSTs and writes rows to the Sheet set inside code.gs (SPREADSHEET_ID).
  */
 export const GOOGLE_SCRIPT_URL =
-    'https://script.google.com/macros/s/AKfycbxxJdkPT-UHc65ySqxiJhtiRdFn94sK4EsaGbi3NurtRU2h1oYn3A4oawAwR4Hbh4eO/exec';
+    'https://script.google.com/macros/s/AKfycbzvjEErtG4plilEmQu-l3PMphxwddX8QCy7Sk3qtCfy6FFnuC2CW0ERxeU1-MydP38r/exec';
 
 export const GOOGLE_SCRIPT_PLACEHOLDER = 'YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL';
 
